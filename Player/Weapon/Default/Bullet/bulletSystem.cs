f6f370cf171117307ff4841627688905449d3bad
Karim Benani <karimcool5900@gmail.com>
2021-05-07 01:16:25 +0200

    timer system for shooting delay (unfinished)

--- Player/Weapon/Default/Bullet/bulletSystem.cs
+++ Player/Weapon/Default/Bullet/bulletSystem.cs
@@ -4,16 +4,19 @@ using System;
 public class bulletSystem : Node2D
 {
     PackedScene rlBullet;
+    Timer t;
 
     public bulletSystem()
     {
         rlBullet = (PackedScene)GD.Load("res://Player/Weapon/Default/Bullet/Instanced/Bullet.tscn");
+        t = GetNode<Timer>("Node/bulletSystem/DELAY");
     }
 
+    /*
     public override void _Ready()
     {
-
     }
+    */
 
     public void shoot()
     {
@@ -23,11 +26,18 @@ public class bulletSystem : Node2D
             var bullet = rlBullet.Instance() as Node2D;
             bullet.Position = GetNode<KinematicBody2D>("../Player").Position + new Vector2(32, -3);
             AddChild(bullet);
+
         }
+    }
+    public async void cooldown()
+    {
+        t.Start();
+        await ToSignal(t, "timeout");
     }
 
     public override void _Process(float delta)
     {
         shoot();
+        cooldown();
     }
 }
